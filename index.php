a1e0adf4ae60fdb2c0806abb9d4b67e8afe62317
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-08-12 22:50:37 -0700

    Freehanded basic structure of site

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Answery- a simple webapp for people who like answering questions</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<body>
+<h1>Answery</h1>
+<p>-a simple webapp for people who like answering random questions-</p>
+<hr>
+
+<?php
+    require("questions.php");
+
+
+
+
+
+
+ ?>
+ Remember to copy your answers if you want to save them after you are done because they are NOT saved.
+</body>
+</html>
---
+++ questions.php
@@ -0,0 +1,39 @@
+<?php
+$shortAnswer = array(
+    "Describe the breed of dog you like best and why you like them.",
+    "Describe the weirdest/most adorable story you have of you when you were a kid",
+    "What is your favorite emoji and why?",
+    "Describe your ideal relationship",
+    "What personality trait do you see the most in yourself and why?"
+);
+
+$multipleChoice = array(
+    array(
+        "question" => "Which of these puppies is cuter?",
+        "text" => "",
+        "image" => "",
+        "text" => "",
+        "image" => ""
+    ),
+    array(
+        "question" => "Pick your favorite condiment",
+        "text" => "",
+        "image" => "",
+        "text" => "",
+        "image" => ""
+    ),
+    array(
+        "question" => "",
+        "text" => "",
+        "image" => "",
+        "text" => "",
+        "image" => ""
+    )
+
+);
+
+$feedback = array();
+
+
+
+ ?>
