e857caf0c0d52acfeca6c033a6e8437715eadeec
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-08-14 21:42:25 -0700

    add cat images

--- index.php
+++ index.php
@@ -9,22 +9,37 @@
 <p id="subtitle">-a simple webapp for people who like answering random questions-</p>
 <hr>
 
-<h2>Would you please insert a question here?</h2>
 
 <?php
     require("questions.php");
 
+//var_dump($_POST);
+    //get Cat
+    $catPath = file_get_contents('http://random.cat/meow');
+    $catPath = json_decode($catPath, true);
+    $catPath = $catPath["file"];
 
+if(isset($_GET["cat"])) {
 
-
+echo '<img id="cat" src="' . $catPath . '"/>';
+} else {
 
 
  ?>
+ <h2>Would you please insert a question here?</h2>
+
  <textarea> </textarea>
+ <?php
+
+ }
+ ?>
+
     <footer>
         <form action="#" method="get">
+            <?php if(!isset($_GET["cat"])) {?>
             <input type="hidden" name="cat" value="true"></input>
             <input type="submit" value="New Question"></input>
+        <?php } ?>
         </form>
     </footer>
 </body>
