fe72f17c87f8161348293ba115144154817e0ddc
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-08-15 10:12:00 -0700

    restructure question arrays into one array for easier random selection

--- questions.php
+++ questions.php
@@ -1,39 +1,40 @@
 <?php
-$shortAnswer = array(
-    "Describe the breed of dog you like best and why you like them.",
-    "Describe the weirdest/most adorable story you have of you when you were a kid",
-    "What is your favorite emoji and why?",
-    "Describe your ideal relationship",
-    "What personality trait do you see the most in yourself and why?"
-);
 
-$multipleChoice = array(
+$questions = array(
+    //Short answer
     array(
-        "question" => "Which of these puppies is cuter?",
-        "text" => "",
-        "image" => "",
-        "text" => "",
-        "image" => ""
+        "Describe the breed of dog you like best and why you like them.",
+        "Describe the weirdest/most adorable story you have of you when you were a kid",
+        "What is your favorite emoji and why?",
+        "Describe your ideal relationship",
+        "What personality trait do you see the most in yourself and why?"
     ),
+    //Multiple choice
     array(
-        "question" => "Pick your favorite condiment",
-        "text" => "",
-        "image" => "",
-        "text" => "",
-        "image" => ""
-    ),
-    array(
-        "question" => "",
-        "text" => "",
-        "image" => "",
-        "text" => "",
-        "image" => ""
+        array(
+            "question" => "Which of these puppies is cuter?",
+            "text" => "",
+            "image" => "",
+            "text" => "",
+            "image" => ""
+        ),
+        array(
+            "question" => "Pick your favorite condiment",
+            "text" => "",
+            "image" => "",
+            "text" => "",
+            "image" => ""
+        ),
+        array(
+            "question" => "",
+            "text" => "",
+            "image" => "",
+            "text" => "",
+            "image" => ""
+        )
     )
-
+    //other type???
 );
-
-$feedback = array();
-
 
 
  ?>
