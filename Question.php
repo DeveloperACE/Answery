a0b864f3eb09a133aef78d36200dcd43770d07f4
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-10-18 17:45:27 -0700

    update readme, comment out some debug stuff and tweak a function to make it more static (???)

--- Question.php
+++ Question.php
@@ -173,7 +173,7 @@ class Question {
             if ($APIOptions[$index]->getType() == OptionType::API) {
                 $output[] = new Option(
                     OptionType::Image,
-                    Question::getValueFromAPI($APIOptions[$index]->getContent(), $APIOptions[$index]->getKey())
+                    $APIOptions[$index]->getContent()->getRawValue()
                 );
             }
         }
@@ -196,17 +196,14 @@ class Question {
     }
 
 
-
-
-
-
+    /*
     public static function var_dump_pre($mixed = null) {
         echo '<pre>';
         var_dump($mixed);
         echo '</pre>';
         return null;
     }
-
+*/
 
 
 
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 A simple site for people who like answering questions
 
 ## To Install Dependencies
-Answery uses the PHP dependency manager [Composer](https://getcomposer.org). To "install" dependencies for this project, run `php composer.phar install` after downloading or cloning the repository
+Answery uses the PHP dependency manager [Composer](https://getcomposer.org). To "install" dependencies for this project, run `php composer.phar install` after downloading or cloning the repository and installing composer
 
 
 ## API's Used
