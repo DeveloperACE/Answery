6075922459975aeecc4e18938dbe871dd3c19845
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2018-01-01 19:44:12 -0800

    add basic API structure

--- api/api.php
+++ api/api.php
@@ -5,8 +5,47 @@
  * Time: 10:19 AM
  */
 
+if (isset($_GET["action"])) {
+
+    $action = $_GET["action"];
+
+    switch ($action){
+        case "random":
+
+
+            break;
+        case "randomdynamic":
+
+
+            break;
+        case "randomstatic":
+
+
+            break;
+        case "byid":
+
+            break;
+        case "all":
+
+
+            break;
+        default:
+            echo "Invalid Parameter. Please see the documentation on github.";
+            break;
+
+    }
+
+
+
+
+
+} else {
+    echo "Invalid Token. Please see the documentation on github.";
+}
+require("data.php");
 class Api
 {
 
-    
+    public static function idIsValid($id) {return is_int($id) && ($id <= count($excitingQuestions + $boringQuestions));}
+
 }
