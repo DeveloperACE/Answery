918d1dbaa6a2f6079582caf6f5180ed2bafd0f79
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2018-01-03 21:57:16 -0800

    add API functionality for documented parameters

--- api/api.php
+++ api/api.php
@@ -5,30 +5,44 @@
  * Time: 10:19 AM
  */
 
+
+require("../data.php");
+
 if (isset($_GET["action"])) {
 
     $action = $_GET["action"];
 
     switch ($action){
         case "random":
+            $questions = $excitingQuestions + $boringQuestions;
 
+            $questionID = mt_rand(0, count($questions)-1);
+            $questionObject = $questions[$questionID]->getJSON();
+
+            echo $questionObject;
 
             break;
         case "randomdynamic":
+
+            $questions = $excitingQuestions;
+            $questionID = mt_rand(0, count($questions)-1);
+            $questionObject = $questions[$questionID]->getJSON();
+
+            echo $questionObject;
 
 
             break;
         case "randomstatic":
 
+            $questions = $boringQuestions;
+            $questionID = mt_rand(0, count($questions)-1);
+            $questionObject = $questions[$questionID]->getJSON();
+
+            echo $questionObject;
 
             break;
-        case "byid":
-
-            break;
-        case "all":
 
 
-            break;
         default:
             echo "Invalid Parameter. Please see the documentation on github.";
             break;
@@ -42,10 +56,14 @@ if (isset($_GET["action"])) {
 } else {
     echo "Invalid Token. Please see the documentation on github.";
 }
-require("data.php");
-class Api
-{
 
-    public static function idIsValid($id) {return is_int($id) && ($id <= count($excitingQuestions + $boringQuestions));}
 
-}
+
+
+//class Api
+//{
+//
+//    public static function idIsValid($id) {return is_int($id) && ($id <= count(self::getAllData()));}
+//
+//
+//}
